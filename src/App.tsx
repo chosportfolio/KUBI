9fda065bc06f9e674a4ac3709e622de7744f5d3e
chosportfolio <chohs5762@gmail.com>
2026-06-10 02:34:32 +0900

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
